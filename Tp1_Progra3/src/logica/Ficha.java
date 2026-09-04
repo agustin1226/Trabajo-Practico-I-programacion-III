e515f09aec9afe91e36b1342de3a8d5f70bb809d
agustin1226 <124695141+agustin1226@users.noreply.github.com>
2026-09-04 06:10:56 -0300

    Refactor Ficha class and improve comments

--- Tp1_Progra3/src/logica/Ficha.java
+++ Tp1_Progra3/src/logica/Ficha.java
@@ -1,44 +1,34 @@
 package logica;
 
-public class Ficha {
-    
+public class Ficha {    
     private int valor;
-
     public Ficha(int valor) {
         this.valor = valor;
     }
-
     public int getValor() {
         return valor;
     }
-
     // Verifica si esta ficha puedefusionarse con otra
     public boolean puedeFusionarseCon(Ficha otra) {
         boolean puede = false;
-
         if (otra != null) {
             int valorOtra = otra.getValor();
-
-            // Regla 1: El 1 se fusiona con el 2 (y viceversa)
+            //El 1 se fusiona con el 2 (y viceversa)
             if ((this.valor == 1 && valorOtra == 2) || (this.valor == 2 && valorOtra == 1)) {
                 puede = true;
             }
-            // Regla 2: De 3 en adelante, solo se fusionan si son iguales
+            //De 3 en adelante, solo se fusionan si son iguales
             else if (this.valor >= 3 && this.valor == valorOtra) {
                 puede = true;
             }
         }
-
         return puede; 
     }
-
     public Ficha fusionar(Ficha otra) {
         Ficha resultado = this;
-
         if (puedeFusionarseCon(otra)) {
             resultado = new Ficha(this.valor + otra.getValor());
-        }
-        
+        }     
         return resultado;
     }
 }
