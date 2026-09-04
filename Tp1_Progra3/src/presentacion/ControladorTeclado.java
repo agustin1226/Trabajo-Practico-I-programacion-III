f01f088159ca07d3f4a9cc68780e878f0413a52b
agustin1226 <124695141+agustin1226@users.noreply.github.com>
2026-09-04 06:08:48 -0300

    Clean up ControladorTeclado.java by removing comments
    Removed comments to clean up the code.

--- Tp1_Progra3/src/presentacion/ControladorTeclado.java
+++ Tp1_Progra3/src/presentacion/ControladorTeclado.java
@@ -6,20 +6,14 @@ import logica.Direccion;
 import logica.JuegoThrees;
 
 public class ControladorTeclado extends KeyAdapter {
-    
-    // El controlador necesita conocer al modelo para mandarle las órdenes
     private JuegoThrees modelo;
-
     public ControladorTeclado(JuegoThrees modelo) {
         this.modelo = modelo;
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // Obtenemos el código de la tecla que el usuario acaba de presionar
         int tecla = e.getKeyCode();
-
-        // Mapeamos las flechas del teclado a nuestro Enum Direccion y llamamos al modelo
         switch (tecla) {
             case KeyEvent.VK_UP:
                 modelo.procesarMovimiento(Direccion.ARRIBA);
