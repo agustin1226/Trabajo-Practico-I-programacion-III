3b50b7e2b3f2f13906e139910d1fd69573b1f604
Agustin Torres <agustin0208torres@gmail.com>
2026-09-06 23:26:48 -0300

    añadi las direcciones con wasd

--- Tp1_Progra3/src/presentacion/ControladorTeclado.java
+++ Tp1_Progra3/src/presentacion/ControladorTeclado.java
@@ -22,15 +22,19 @@ public class ControladorTeclado extends KeyAdapter {
         // Mapeamos las flechas del teclado a nuestro Enum Direccion y llamamos al modelo
         switch (tecla) {
             case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:	
                 modelo.procesarMovimiento(Direccion.ARRIBA);
                 break;
             case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:	
                 modelo.procesarMovimiento(Direccion.ABAJO);
                 break;
             case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:	
                 modelo.procesarMovimiento(Direccion.IZQUIERDA);
                 break;
             case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:	
                 modelo.procesarMovimiento(Direccion.DERECHA);
                 break;
         }
